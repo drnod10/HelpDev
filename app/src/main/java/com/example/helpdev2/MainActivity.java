c9d0e901394b4fb60cbb5239e0f67b717835fea6
Gabriel <cfaltribim@gmail.com>
2020-06-10 14:23:24 -0300

    tela inicial

--- app/src/main/java/com/example/helpdev2/MainActivity.java
+++ app/src/main/java/com/example/helpdev2/MainActivity.java
@@ -50,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btlogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TelaInicialActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
