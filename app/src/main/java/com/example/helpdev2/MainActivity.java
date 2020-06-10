8856e39b42ac0c1692f02117b5e678ca449c053e
Gabriel <cfaltribim@gmail.com>
2020-06-09 22:55:49 -0300

    Telas iniciais

--- app/src/main/java/com/example/helpdev2/MainActivity.java
+++ app/src/main/java/com/example/helpdev2/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.helpdev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btlogar,btcadastar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btlogar = findViewById(R.id.btlogar);
+        btcadastar = findViewById(R.id.btcadastrar);
+
+        btcadastar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
