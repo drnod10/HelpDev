6c9896809ceed470e2f8b8bd40f9ef31e07a3491
Renato <drnodd1@gmail.com>
2020-06-09 23:07:30 -0300

    Criação dos arquivos

--- app/src/main/java/com/example/helpdev2/MainActivity.java
+++ app/src/main/java/com/example/helpdev2/MainActivity.java
@@ -2,14 +2,15 @@ package com.example.helpdev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btlogar,btcadastar;
+    private Activity getActivity;
+    Button btlogar, btcadastar, btcancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         btlogar = findViewById(R.id.btlogar);
         btcadastar = findViewById(R.id.btcadastrar);
+        btcancelar = findViewById(R.id.btcancelar);
 
         btcadastar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
                 Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
                 startActivity(intent);
+            }
 
+        });
+        btcancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity.finish();
             }
         });
-
-
     }
 }
