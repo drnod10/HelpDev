70ab7db7a9b0bbf61a89b2808ff9fed65779a992
Gabriel <cfaltribim@gmail.com>
2020-06-11 17:29:48 -0300

    atualizando usuario !

--- app/src/main/java/com/example/helpdev2/PerfilActivity.java
+++ app/src/main/java/com/example/helpdev2/PerfilActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -14,7 +16,7 @@ import android.widget.EditText;
 public class PerfilActivity extends AppCompatActivity {
 
     EditText nome,apelido,email,telefone;
-    Button btcancelar,btdeletar;
+    Button btcancelar,btdeletar,btatualizar;
     SQLiteDatabase db;
 
 
@@ -30,19 +32,21 @@ public class PerfilActivity extends AppCompatActivity {
 
         btcancelar = findViewById(R.id.btcancelarperfil);
         btdeletar = findViewById(R.id.btdeletar);
+        btatualizar = findViewById(R.id.btcadastro);
+        final DialogInterface.OnClickListener AtualizarUsuario,encerrapagina;
+
+        final Cliente c = getIntent().getExtras().getParcelable("cliente");
+        final Integer codigo = c.getCodigo();
 
         try {
 
-            Cliente c = getIntent().getExtras().getParcelable("cliente");
-            Integer codigo = c.getCodigo();
-
             db = openOrCreateDatabase("banco_dados", Context.MODE_PRIVATE,null);
-            System.out.println(codigo);
+
             Cursor res = db.rawQuery("select nome,apelido,email,telefone from usuarios WHERE id = "+codigo+"", null);
 
             if (res.getCount() > 0) {
                 res.moveToFirst();
-                System.out.println(res.getString(0));
+
                 nome.setText(res.getString(0));
                 apelido.setText(res.getString(1));
                 email.setText(res.getString(2));
@@ -65,12 +69,72 @@ public class PerfilActivity extends AppCompatActivity {
                                 .setNegativeButton("Não",null)
                                 .show();
                     }
+
                 });
+
+                encerrapagina = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        try{
+                            finish();
+                        }catch(Exception e){
+                            AlertDialog.Builder dialogo = new AlertDialog.Builder(PerfilActivity.this);
+                            dialogo.setTitle("Aviso");
+                            dialogo.setMessage("Eroor !")
+                                    .setNeutralButton("OK",null)
+                                    .show();
+                        }
+                    }
+                };
+
+                AtualizarUsuario = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        try{
+                            String nom = nome.getText().toString();
+                            String apel = apelido.getText().toString();
+                            String em = email.getText().toString();
+                            String tel = telefone.getText().toString();
+
+                            db.execSQL("update usuarios set nome ='"+nom+"' , apelido ='"+apel+"' , email ='"+em+"', telefone ='"+tel+"' where id = " + codigo);
+                            AlertDialog.Builder dialogo = new AlertDialog.Builder(PerfilActivity.this);
+                            dialogo.setTitle("Aviso");
+                            dialogo.setMessage("Usuario Atualizado com Sucesso !")
+                                    .setNeutralButton("OK",encerrapagina)
+                                    .show();
+                        }catch(Exception e){
+                            AlertDialog.Builder dialogo = new AlertDialog.Builder(PerfilActivity.this);
+                            dialogo.setTitle("Aviso");
+                            dialogo.setMessage("Eroor !")
+                                    .setNeutralButton("OK",null)
+                                    .show();
+                        }
+                    }
+                };
+
+
+                btatualizar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        AlertDialog.Builder dialogo = new AlertDialog.Builder(PerfilActivity.this);
+                        dialogo.setTitle("Aviso");
+                        dialogo.setMessage("Deseja Alterar os Dados?")
+                                .setPositiveButton("Sim", AtualizarUsuario)
+                                .setNegativeButton("Não",null)
+                                .show();
+                    }
+                });
+
 
             }else {
                 AlertDialog.Builder dialogo = new AlertDialog.Builder(PerfilActivity.this);
                 dialogo.setTitle("Aviso");
-                dialogo.setMessage("Erro ao listar perfil")
+                dialogo.setMessage("Problema interno ao listar !")
+                        .setNeutralButton("OK",null)
                         .show();
             }
 
@@ -79,4 +143,5 @@ public class PerfilActivity extends AppCompatActivity {
             }
 
     }
+
 }
