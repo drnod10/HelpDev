e28da797ef5c187793db9b71c4f5a92cda2c738f
Renato <drnodd1@gmail.com>
2020-06-10 22:36:03 -0300

    Tela de Notícia

--- app/src/main/java/com/example/helpdev2/PerfilActivity.java
+++ app/src/main/java/com/example/helpdev2/PerfilActivity.java
@@ -3,18 +3,29 @@ package com.example.helpdev2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class PerfilActivity extends AppCompatActivity {
 
     EditText nome;
+    Button btcancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
+        btcancelar = findViewById(R.id.btcancelarperfil);
         nome = findViewById(R.id.nomeperfil);
+
+        btcancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         try {
 
