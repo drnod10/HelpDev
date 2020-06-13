a460cd2665daba3f19e402d62556dbbae29b132c
Renato <drnodd1@gmail.com>
2020-06-13 14:44:44 -0300

    Select Notícias

--- app/src/main/java/com/example/helpdev2/NoticiasActivity.java
+++ app/src/main/java/com/example/helpdev2/NoticiasActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.sql.SQLOutput;
 
 public class NoticiasActivity extends AppCompatActivity {
 
-    Button btvoltar,btprox,btant;
+    Button btvoltar, btprox, btant;
     SQLiteDatabase db;
+    TextView titulo, texto;
+    int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +28,18 @@ public class NoticiasActivity extends AppCompatActivity {
         btvoltar = findViewById(R.id.btvoltar);
         btant = findViewById(R.id.btanterior);
         btprox = findViewById(R.id.btproximo);
-
+        titulo = findViewById(R.id.titulonoticia);
+        texto = findViewById(R.id.textonoticia);
 
         try {
+
             db = openOrCreateDatabase("banco_dados",
                     Context.MODE_PRIVATE, null);
-            db.execSQL("create table if not exists "+
+            db.execSQL("create table if not exists " +
                     "noticias(id integer primary key " +
                     "autoincrement, titulo text not null, texto text " +
                     "not null, " + "comentarios text not null)");
             System.out.println("Banco de Dados Criado com Sucesso!");
-
 
             String apelido = "PROJEÇÃO MORREU DPS QUE O AKL SAIU !!!";
             String email = "ESTUPIDOS !";
@@ -43,25 +48,41 @@ public class NoticiasActivity extends AppCompatActivity {
             System.out.println(apelido);
             System.out.println(email);
 
+            db.execSQL("insert into noticias(id, titulo, texto, comentarios) values (null,'" + apelido + "','" + email + "','" + com + "')");
 
+            Cursor res = db.rawQuery("select titulo,texto from noticias", null);
 
-            db.execSQL("insert into noticias(id, titulo, texto, comentarios) values (null,'"+apelido+"','"+email+"','"+com+"')");
+            if (res.getCount() > 0) {
 
+                res.moveToFirst();
 
-            btvoltar.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                }
-            });
+                titulo.setText(res.getString(0));
+                texto.setText(res.getString(1));
+            }
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
 
-        }catch (Exception e) {
-            System.out.println(e.toString());
-        }
+                btant.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        contador--;
+                    }
+                });
 
+                btvoltar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                });
 
-
-
-
+                btprox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        contador++;
+                    }
+                });
+            }
     }
-}
+
