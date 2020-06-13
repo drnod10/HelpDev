fcc190ac2fbf094340c9325fe211c3da0290995c
Renato <drnodd1@gmail.com>
2020-06-13 16:33:16 -0300

    Tela códigos

--- app/src/main/java/com/example/helpdev2/CodigoActivity.java
+++ app/src/main/java/com/example/helpdev2/CodigoActivity.java
@@ -3,12 +3,26 @@ package com.example.helpdev2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class CodigoActivity extends AppCompatActivity {
+
+    Button btvoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_codigo);
+
+        btvoltar = findViewById(R.id.voltarduvida);
+
+        btvoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
